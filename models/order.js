27cdac447b51fc8074091827f3bff645b41da20a
gmluqa <gmluqa@protonmail.com>
2022-11-05 13:03:35 +0100

    asocs in models

--- models/order.js
+++ models/order.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Order.hasMany(models.Product)
+      Order.hasMany(models.User)
+
     }
   }
   Order.init({
