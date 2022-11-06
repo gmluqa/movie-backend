53afa28f2604a129566314037b1f3170be653ffc
gmluqa <gmluqa@protonmail.com>
2022-11-06 13:00:15 +0100

    10 order seeds made

--- seeders/20221105130756-orders.js
+++ seeders/20221105130756-orders.js
@@ -16,20 +16,50 @@ module.exports = {
       UpdatedAt: '1998-06-10 06:40:10'
     },
     {
-      User_ID: 1,
+      User_ID: 3,
+      Product_ID: 2,
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      User_ID: 4,
       Product_ID: 2,
       CreatedAt: '1998-06-10 06:40:10',
       UpdatedAt: '1998-06-10 06:40:10'
     },
     {
       User_ID: 5,
-      Product_ID: 1,
+      Product_ID: 3,
       CreatedAt: '1998-06-10 06:40:10',
       UpdatedAt: '1998-06-10 06:40:10'
     },
     {
-      User_ID: 2,
-      Product_ID: 1,
+      User_ID: 6,
+      Product_ID: 4,
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      User_ID: 7,
+      Product_ID: 5,
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      User_ID: 8,
+      Product_ID: 6,
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      User_ID: 9,
+      Product_ID: 7,
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      User_ID: 10,
+      Product_ID: 10,
       CreatedAt: '1998-06-10 06:40:10',
       UpdatedAt: '1998-06-10 06:40:10'
     }], {});
