4fafe2f28aad81d413232f56a7e621668a4e5b3a
gmluqa <gmluqa@protonmail.com>
2022-11-13 12:33:38 +0100

    Email middleware works

--- middleware/user.middleware.js
+++ middleware/user.middleware.js
@@ -1,6 +1,22 @@
-const checkIfEmailValid = (req, res, next) => {
-    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    next()
+const models = require("../models/index")
+
+const checkIfEmailValid = async (req, res, next) => {
+    // First will check if email is valid
+    // Then will check if email is already registered or not
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (req.body.Email.match(emailRegex)) {
+        const findUserWithEmail = await models.User.findOne({
+            where: {
+                Email: req.body.Email
+            }
+        })
+        findUserWithEmail ? res.status(403).send({ message: "Email is already registered!" }) : next()
+    } else {
+        res.status(400).json({ message: "Invalid formatting!" })
+    }
+
+
 }
 
 module.exports = {
