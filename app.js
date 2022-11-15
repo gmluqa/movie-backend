2733b3c7be694119debc0f01a00355cac22becba
gmluqa <gmluqa@protonmail.com>
2022-11-15 12:36:49 +0100

    Added cors as middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 
 
 //Routes
@@ -8,6 +10,7 @@ const orderRouter = require("./routers/order.router.js");
 const userRouter = require("./routers/user.router.js");
 
 // Middleware
+app.use(cors())
 app.use(express.json())
 app.use("/product", productRouter);
 app.use("/order", orderRouter);
