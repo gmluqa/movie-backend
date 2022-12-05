efeecdbcba4b5bc7602809ab40fa067aa82c212f
Manel <quemonmanel@gmail.com>
2022-12-05 02:02:41 +0100

    more films

--- seeders/20221105130258-products.js
+++ seeders/20221105130258-products.js
@@ -93,11 +93,80 @@ module.exports = {
       UpdatedAt: '1998-06-10 06:40:10'
     },
     {
+      Name: 'The Fist Of The North Star',
+      Poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pqdyv9Z4no4cElRtbpMw17STpOH.jpg',
+      ContentType: "Movie",
+      rating: 9,
+      Genre: "Magic",
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      Name: 'Angels Egg',
+      Poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/s65qAjKy6JqLqQUDj3Liuih0r1D.jpg',
+      ContentType: "Movie",
+      rating: 9,
+      Genre: "Magic",
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      Name: 'Appleseed',
+      Poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/psYU0cYB4DaQRGg4v8Dtt7RhXfa.jpg',
+      ContentType: "Movie",
+      rating: 9,
+      Genre: "Action",
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      Name: 'Mobile Suit Gundam I',
+      Poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/kzyMAqagPK6wuvDzA5FeJU6n71Y.jpg',
+      ContentType: "Movie",
+      rating: 9,
+      Genre: "Sci-Fiction",
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      Name: 'Wicked City',
+      Poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/efxHZF6uDCIATKVxTuobQFpJarp.jpg',
+      ContentType: "Series",
+      rating: 8,
+      Genre: "Comedy",
+      EpInWeek: false,
+      TheatrePass: false,
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      Name: 'Demon City',
+      Poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/htCzmp0gEvUCc2jfQMzfbwSGN4L.jpg',
+      ContentType: "Series",
+      rating: 8,
+      Genre: "Comedy",
+      EpInWeek: false,
+      TheatrePass: false,
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
+      Name: 'Dragon Quest',
+      Poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1ku4wBlLxMecZUoLMbxqyVexGft.jpg',
+      ContentType: "Series",
+      rating: 8,
+      Genre: "Comedy",
+      EpInWeek: false,
+      TheatrePass: false,
+      CreatedAt: '1998-06-10 06:40:10',
+      UpdatedAt: '1998-06-10 06:40:10'
+    },
+    {
       Name: 'Doomed Megalopolis',
       Poster: 'https://i.pinimg.com/originals/3f/07/7b/3f077b9f2f2321d29a2b4edd4f2e5d1f.jpg',
       ContentType: "Series",
       rating: 10,
-      Genre: "Comedy",
+      Genre: "Horror, Fantasy",
       EpInWeek: true,
       TheatrePass: true,
       CreatedAt: '1998-06-10 06:40:10',
